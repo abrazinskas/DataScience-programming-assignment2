f272feec3e8a7accbe7bd14d83831a042cb9f6c2
ixlan <bulletdll@gmail.com>
2015-01-01 21:06:51 +1300

    a bit more comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,10 @@
-## Two functions permit a use of cached versions of inversed matrices.
-
- 
+## This file contains two functions that permit a use of previously cached inversed matrices in order
+## to save computation resources.
 
  ## makeCacheMatrix is a function that wraps a matrix into a list containing other useful methods that
  ## are later used by cacheSolve.
+ ## parameters:
+ ## x: a matrix that later will be inverted, under the assumption that x is invertable. 
 makeCacheMatrix <- function(x = matrix()) {
   c <- NULL
   set <- function(y) {
@@ -19,8 +20,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve detects if a matrix was previously inversted and in case if it was, then.
-## the cached version is returned, otherwise the inverse operation is performed.
+## cacheSolve detects if a matrix was previously inversted and in case if it was then
+## the cached version is returned, otherwise the inverse operation is performed and a freshly
+## inversed matrix is returned.
+## parameters: 
+## x: a list that was returned by makeCacheMatrix function above.
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   c <- x$getCache()
